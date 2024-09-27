9e6282fbc8655b977b4a8e0aeac12fe2456befda
Aaron Garcia <axyscreativeagency@gmail.com>
2024-09-26 21:06:35 -0600

    header/nav progress

--- src/static/javascript/global/magnet.js
+++ src/static/javascript/global/magnet.js
@@ -9,25 +9,25 @@ if (mqMouse.matches) {
         const mx = e.clientX - pos.left - pos.width / 2;
         const my = e.clientY - pos.top - pos.height / 2;
 
-        this.style.transform = `translate(${mx * 0.48}px, ${my * 0.48}px)`;
+        this.style.translate = `${mx * 0.48}px ${my * 0.48}px`;
 
         if (this.classList.contains("magnet-weak")) {
-          this.style.transform = `translate(${mx * 0.12}px, ${my * 0.12}px)`;
+          this.style.translate = `${mx * 0.12}px ${my * 0.12}px`;
         }
 
         if (this.classList.contains("magnet-strong")) {
-          this.style.transform = `translate(${mx * 0.96}px, ${my * 0.96}px)`;
+          this.style.translate = `${mx * 0.96}px ${my * 0.96}px`;
         }
 
         if (this.classList.contains("magnet-wide-btn")) {
-          this.style.transform = `translate(${mx * 0.48}px, ${my * 0.96}px)`;
+          this.style.translate = `${mx * 0.48}px ${my * 0.96}px`;
         }
       })
     );
 
     document.querySelectorAll(".magnet").forEach((el) =>
       el.addEventListener("mouseleave", function () {
-        this.style.transform = "translate(0, 0)";
+        this.style.translate = "0 0";
       })
     );
   };
