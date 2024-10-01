ce257f4782b9dd05d2fd6e8eade7ae06df1fb43e
Aaron Garcia <axyscreativeagency@gmail.com>
2024-09-30 22:13:01 -0600

    nav progress

--- src/static/javascript/global/magnet.js
+++ src/static/javascript/global/magnet.js
@@ -1,49 +1,49 @@
 let mqMouse = window.matchMedia("(hover: hover) and (pointer: fine)");
 
-if (mqMouse.matches) {
-  const magnetEffect = () => {
-    document.querySelectorAll(".magnet").forEach((el) =>
-      el.addEventListener("mousemove", function (e) {
-        const pos = this.getBoundingClientRect();
-        const mx = e.clientX - pos.left - pos.width / 2;
-        const my = e.clientY - pos.top - pos.height / 2;
+// if (mqMouse.matches) {
+const magnetEffect = () => {
+  document.querySelectorAll(".magnet").forEach((el) =>
+    el.addEventListener("mousemove", function (e) {
+      const pos = this.getBoundingClientRect();
+      const mx = e.clientX - pos.left - pos.width / 2;
+      const my = e.clientY - pos.top - pos.height / 2;
 
-        this.style.translate = `${mx * 0.48}px ${my * 0.48}px`;
+      this.style.translate = `${mx * 0.48}px ${my * 0.48}px`;
 
-        if (this.classList.contains("magnet-weak")) {
-          this.style.translate = `${mx * 0.12}px ${my * 0.12}px`;
-        }
+      if (this.classList.contains("magnet-weak")) {
+        this.style.translate = `${mx * 0.12}px ${my * 0.12}px`;
+      }
 
-        if (this.classList.contains("magnet-strong")) {
-          this.style.translate = `${mx * 0.96}px ${my * 0.96}px`;
-        }
+      if (this.classList.contains("magnet-strong")) {
+        this.style.translate = `${mx * 0.96}px ${my * 0.96}px`;
+      }
 
-        if (this.classList.contains("magnet-wide-btn")) {
-          this.style.translate = `${mx * 0.48}px ${my * 0.96}px`;
-        }
-      })
-    );
+      if (this.classList.contains("magnet-wide-btn")) {
+        this.style.translate = `${mx * 0.48}px ${my * 0.96}px`;
+      }
+    })
+  );
 
-    document.querySelectorAll(".magnet").forEach((el) =>
-      el.addEventListener("mouseleave", function () {
-        this.style.translate = "0 0";
-      })
-    );
-  };
+  document.querySelectorAll(".magnet").forEach((el) =>
+    el.addEventListener("mouseleave", function () {
+      this.style.translate = "0 0";
+    })
+  );
+};
 
-  const magnetText = (() => {
-    document.querySelectorAll(".magnet-text").forEach((element) => {
-      const words = element.innerText.split(" ");
-      const wrappedWords = words.map((word) => {
-        const wrappedLetters = word
-          .split("")
-          .map((letter) => `<span class="magnet">${letter}</span>`)
-          .join("");
-        return `<span class="word">${wrappedLetters}</span>`;
-      });
-      element.innerHTML = wrappedWords.join(" ");
+const magnetText = (() => {
+  document.querySelectorAll(".magnet-text").forEach((element) => {
+    const words = element.innerText.split(" ");
+    const wrappedWords = words.map((word) => {
+      const wrappedLetters = word
+        .split("")
+        .map((letter) => `<span class="magnet">${letter}</span>`)
+        .join("");
+      return `<span class="word">${wrappedLetters}</span>`;
     });
-  })();
+    element.innerHTML = wrappedWords.join(" ");
+  });
+})();
 
-  magnetEffect();
-}
+magnetEffect();
+// }
