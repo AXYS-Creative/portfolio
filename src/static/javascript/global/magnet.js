784f92628c0b1354f74a71a70d172dd6ed71f8f4
Aaron Garcia <axyscreativeagency@gmail.com>
2024-10-12 07:49:37 -0600

    reusable hero/prefooter, magnet text fixed for mobile removal

--- src/static/javascript/global/magnet.js
+++ src/static/javascript/global/magnet.js
@@ -1,36 +1,6 @@
 let mqMouse = window.matchMedia("(hover: hover) and (pointer: fine)");
 
-// if (mqMouse.matches) {
-const magnetEffect = () => {
-  document.querySelectorAll(".magnet").forEach((el) =>
-    el.addEventListener("mousemove", function (e) {
-      const pos = this.getBoundingClientRect();
-      const mx = e.clientX - pos.left - pos.width / 2;
-      const my = e.clientY - pos.top - pos.height / 2;
-
-      this.style.translate = `${mx * 0.48}px ${my * 0.48}px`;
-
-      if (this.classList.contains("magnet-weak")) {
-        this.style.translate = `${mx * 0.12}px ${my * 0.12}px`;
-      }
-
-      if (this.classList.contains("magnet-strong")) {
-        this.style.translate = `${mx * 0.96}px ${my * 0.96}px`;
-      }
-
-      if (this.classList.contains("magnet-wide-btn")) {
-        this.style.translate = `${mx * 0.48}px ${my * 0.96}px`;
-      }
-    })
-  );
-
-  document.querySelectorAll(".magnet").forEach((el) =>
-    el.addEventListener("mouseleave", function () {
-      this.style.translate = "0 0";
-    })
-  );
-};
-
+// Placed above magnetEffect and outside of if statement for mobile
 const magnetText = (() => {
   document.querySelectorAll(".magnet-text").forEach((element) => {
     const words = element.innerText.split(" ");
@@ -45,5 +15,36 @@ const magnetText = (() => {
   });
 })();
 
-magnetEffect();
-// }
+if (mqMouse.matches) {
+  const magnetEffect = () => {
+    document.querySelectorAll(".magnet").forEach((el) =>
+      el.addEventListener("mousemove", function (e) {
+        const pos = this.getBoundingClientRect();
+        const mx = e.clientX - pos.left - pos.width / 2;
+        const my = e.clientY - pos.top - pos.height / 2;
+
+        this.style.translate = `${mx * 0.48}px ${my * 0.48}px`;
+
+        if (this.classList.contains("magnet-weak")) {
+          this.style.translate = `${mx * 0.12}px ${my * 0.12}px`;
+        }
+
+        if (this.classList.contains("magnet-strong")) {
+          this.style.translate = `${mx * 0.96}px ${my * 0.96}px`;
+        }
+
+        if (this.classList.contains("magnet-wide-btn")) {
+          this.style.translate = `${mx * 0.48}px ${my * 0.96}px`;
+        }
+      })
+    );
+
+    document.querySelectorAll(".magnet").forEach((el) =>
+      el.addEventListener("mouseleave", function () {
+        this.style.translate = "0 0";
+      })
+    );
+  };
+
+  magnetEffect();
+}
