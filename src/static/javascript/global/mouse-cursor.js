469e4017447ae967e0e1cd474e80c2200e5c72dc
Aaron Garcia <axyscreativeagency@gmail.com>
2024-10-23 19:49:44 -0600

    tooltip progress

--- src/static/javascript/global/mouse-cursor.js
+++ src/static/javascript/global/mouse-cursor.js
@@ -1,8 +1,11 @@
 if (window.matchMedia("(pointer: fine) and (hover: hover)").matches) {
   const cursor = document.querySelector(".mouse-cursor"),
-    workItem = document.querySelectorAll(".work-item"),
+    tooltip = document.querySelector(".mouse-cursor__tooltip");
+
+  const workItem = document.querySelectorAll(".work-item"),
     workShowcase = document.querySelectorAll(".work-item__showcase-img"),
-    workItemLogo = document.querySelectorAll(".work-item__logo");
+    workItemLogo = document.querySelectorAll(".work-item__logo"),
+    tooltipTargets = document.querySelectorAll(".tooltip-target");
 
   const imageSources = [];
   workShowcase.forEach((img) => imageSources.push(img.src));
@@ -34,43 +37,27 @@ if (window.matchMedia("(pointer: fine) and (hover: hover)").matches) {
       Math.min(maxRotation, Math.abs(velocity) * maxRotation) *
       Math.sign(velocity);
 
-    cursor.querySelectorAll(".mouse-cursor__image").forEach((img) => {
-      img.style.translate = `calc(${e.clientX}px - 50%) calc(${e.clientY}px - 50%)`;
-      img.style.rotate = `0deg`; // Reset rotation to neutral by default
-    });
-
-    const activeImage = cursor.querySelector(
-      ".mouse-cursor__image[style*='opacity: 1']"
-    );
-    if (activeImage) {
-      activeImage.style.rotate = `${-rotationAngle}deg`;
-    }
+    cursor.style.translate = `calc(${e.clientX}px - 50%) calc(${e.clientY}px - 50%)`;
+    cursor.style.rotate = `${-rotationAngle}deg`;
 
     clearTimeout(inactivityTimeout);
     inactivityTimeout = setTimeout(() => {
-      if (activeImage) {
-        activeImage.style.rotate = `0deg`;
-      }
+      cursor.style.rotate = `0deg`;
     }, 100); // Adjust this delay if needed
   });
 
+  // Work Items Images
   workItem.forEach((el, index) => {
+    const activeImage = cursor.querySelectorAll(".mouse-cursor__image")[index];
+
     el.addEventListener("mousemove", () => {
-      cursor.classList.add("img-active");
-
-      cursor.querySelectorAll(".mouse-cursor__image").forEach((img) => {
-        img.style.opacity = 0;
-      });
-
-      // Show only the related image for the hovered work item
-      const activeImage = cursor.querySelectorAll(".mouse-cursor__image")[
-        index
-      ];
-      activeImage.style.opacity = 1;
+      cursor.classList.add("show-cursor");
+      activeImage.classList.add("show-img");
     });
 
     el.addEventListener("mouseleave", () => {
-      cursor.classList.remove("img-active");
+      cursor.classList.remove("show-cursor");
+      activeImage.classList.remove("show-img");
     });
   });
 
@@ -90,6 +77,24 @@ if (window.matchMedia("(pointer: fine) and (hover: hover)").matches) {
     // Ensure the class is removed when leaving the entire work item
     item.addEventListener("mouseleave", () => {
       cursor.classList.remove("cursor-contrast");
+    });
+  });
+
+  // Tootip
+  tooltipTargets.forEach((el) => {
+    // let hideTooltipTimeout;
+
+    el.addEventListener("mouseover", () => {
+      // clearTimeout(hideTooltipTimeout);
+      tooltip.classList.add("show-tooltip");
+      tooltip.innerHTML = el.getAttribute("data-tooltip-title");
+    });
+
+    el.addEventListener("mouseleave", () => {
+      tooltip.classList.remove("show-tooltip");
+      // hideTooltipTimeout = setTimeout(() => {
+      // tooltip.innerHTML = "";
+      // }, 2000);
     });
   });
 
