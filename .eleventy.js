1759403ab6b235edad5ba13a812b9a1916ec94d1
Aaron Garcia <axyscreativeagency@gmail.com>
2024-10-30 20:46:59 -0600

    reverting passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -32,12 +32,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
-
-  eleventyConfig.addPassthroughCopy("src/static/documents");
-  eleventyConfig.addPassthroughCopy("src/static/fonts");
-  eleventyConfig.addPassthroughCopy("src/static/img");
-  eleventyConfig.addPassthroughCopy("src/static/javascript");
-  eleventyConfig.addPassthroughCopy("src/static/styles/styles.css");
+  eleventyConfig.addPassthroughCopy("src/static");
 
   //
   // Custom Functions
