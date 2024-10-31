f9e0d4084d51706cdcd3b64c611d6e9757381bf7
Aaron Garcia <axyscreativeagency@gmail.com>
2024-10-30 20:33:50 -0600

    only passing through styles.css vs whole styles folder

--- .eleventy.js
+++ .eleventy.js
@@ -29,10 +29,15 @@ module.exports = function (eleventyConfig) {
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
   });
-  eleventyConfig.addPassthroughCopy("src/static");
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+
+  eleventyConfig.addPassthroughCopy("src/static/documents");
+  eleventyConfig.addPassthroughCopy("src/static/fonts");
+  eleventyConfig.addPassthroughCopy("src/static/img");
+  eleventyConfig.addPassthroughCopy("src/static/javascript");
+  eleventyConfig.addPassthroughCopy("src/static/styles/styles.css");
 
   //
   // Custom Functions
