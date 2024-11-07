9d190a496059a31c954d272440087adf438a1712
Aaron Garcia <axyscreativeagency@gmail.com>
2024-11-06 22:39:53 -0700

    tooltip on axyscreative link, mouse cursor cleanup

--- src/static/javascript/global/mouse-cursor.js
+++ src/static/javascript/global/mouse-cursor.js
@@ -6,7 +6,6 @@ if (window.matchMedia("(pointer: fine) and (hover: hover)").matches) {
 
   const workItem = document.querySelectorAll(".work-item"),
     workShowcase = document.querySelectorAll(".work-item__showcase-img"),
-    workItemLogo = document.querySelectorAll(".work-item__logo"),
     tooltipTargets = document.querySelectorAll(".tooltip-target");
 
   const imageSources = [];
@@ -48,64 +47,45 @@ if (window.matchMedia("(pointer: fine) and (hover: hover)").matches) {
     }, 100); // Adjust this delay if needed
   });
 
-  // Work Items Images
+  // Work items mouse cursor images
   workItem.forEach((el, index) => {
     const activeImage = cursor.querySelectorAll(".mouse-cursor__image")[index];
 
     el.addEventListener("mousemove", () => {
-      cursor.classList.add("show-cursor");
       activeImage.classList.add("show-img");
     });
 
     el.addEventListener("mouseleave", () => {
-      cursor.classList.remove("show-cursor");
       activeImage.classList.remove("show-img");
+      activeImage.classList.remove("hide-within");
     });
-  });
 
-  workItem.forEach((item) => {
-    // Add the class when entering any child element
-    item.querySelectorAll("img, ul, a").forEach((child) => {
+    // Hide for children elements (logo, badges, cta)
+    el.querySelectorAll("img, ul, a").forEach((child) => {
       child.addEventListener("mouseenter", () => {
-        cursor.classList.add("cursor-contrast");
+        activeImage.classList.add("hide-within");
       });
 
-      // Remove the class when leaving any child element
       child.addEventListener("mouseleave", () => {
-        cursor.classList.remove("cursor-contrast");
+        activeImage.classList.remove("hide-within");
       });
-    });
-
-    // Ensure the class is removed when leaving the entire work item
-    item.addEventListener("mouseleave", () => {
-      cursor.classList.remove("cursor-contrast");
     });
   });
 
   // Tootip
   tooltipTargets.forEach((el) => {
-    // let hideTooltipTimeout;
-
     el.addEventListener("mouseover", () => {
-      // clearTimeout(hideTooltipTimeout);
       tooltip.classList.add("show-tooltip");
       tooltip.innerHTML = el.getAttribute("data-tooltip-title");
     });
 
     el.addEventListener("mouseleave", () => {
       tooltip.classList.remove("show-tooltip");
-      // hideTooltipTimeout = setTimeout(() => {
-      // tooltip.innerHTML = "";
-      // }, 2000);
     });
   });
 
-  // Hide Mouse cursor on Safari ... glitched the animation
-
+  // Hide Mouse cursor on Safari, choppy transition
   if (isSafari()) {
     cursor.style.display = "none";
-    // cursor.style.transition = "none"; // Not working
-
-    // workShowcase.forEach((img) => (img.style.transition = "none"));
   }
 }
