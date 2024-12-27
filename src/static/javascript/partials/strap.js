e52effad81fd357e5019a9171dd1ac5f0b727b66
Aaron Garcia <axyscreativeagency@gmail.com>
2024-12-27 14:52:03 -0700

    clean up strap hover/focus styles

--- src/static/javascript/partials/strap.js
+++ src/static/javascript/partials/strap.js
@@ -1,30 +1,11 @@
 import { root } from "../utility.js";
 
-const strap = document.querySelector(".strap"),
-  strapIcon = document.querySelector(".strap-icon");
+const strap = document.querySelector(".strap");
 
 const updateIconWidth = () => {
   let strapWidth = strap.getBoundingClientRect().width;
   root.style.setProperty("--strap-width", `${strapWidth}px`);
 };
 
-updateIconWidth(); // Invoke on page load
-window.addEventListener("resize", updateIconWidth); // Invoke on window resize
-
-// Strap hover
-strap.addEventListener("mousemove", () => {
-  strapIcon.classList.add("strap-hover");
-});
-
-strap.addEventListener("mouseleave", () => {
-  strapIcon.classList.remove("strap-hover");
-});
-
-// Strap focus-within
-strap.addEventListener("focusin", () => {
-  strapIcon.classList.add("strap-hover");
-});
-
-strap.addEventListener("focusout", () => {
-  strapIcon.classList.remove("strap-hover");
-});
+updateIconWidth();
+window.addEventListener("resize", updateIconWidth);
